2f37ddf1d249ba3825c4aaf6ee3450cd722549f1
Akimasa Takeuchi <akimasa.takeuchi@gaiax.com>
2015-06-03 20:46:10 +0900

    edit many

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"read_comp/tarreader"
 )
 
 func main() {
@@ -43,7 +44,7 @@ OPTIONS:
 		}
 
 		i := c.Args()
-		readFile(i)
+		go readFile(i)
 	}
 	app.Run(os.Args)
 }
@@ -79,48 +80,6 @@ func readFile(args []string) {
 	defer fileReader.Close()
 	
 	tr := tar.NewReader(fileReader)
-	for {
-		hdr, err = tr.Next()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal("Can't read hdr \n")
-			break
-		}
-		if default_Regexp.MatchString(hdr.Name) {
-			fmt.Println(hdr.Name)
-			fileinfo = hdr.FileInfo()
-			func() {
-				mkdir_name, _ := filepath.Split(hdr.Name)
-				changeDir(dd)
-
-				if err = os.MkdirAll(mkdir_name, fileinfo.Mode()); err != nil {
-					log.Fatal(err)
-				}
-				if hdr.Typeflag == '0' {
-					wfile, werr := os.Create(hdr.Name)
-					if werr != nil {
-						log.Fatal(werr)
-					}
-					defer wfile.Close()
-
-					body = make([]byte, 8192)
-					for {
-						c, rerr := tr.Read(body)
-						if c == 0 {
-							break
-						}
-						if rerr != nil {
-							log.Fatal(rerr)
-						}
-						wfile.Write(body[:c])
-					}
-				}
-			}()
-			changeDir(dir_name)
-		}
-	}
 }
 
 func changeDir(dir_name string) {
