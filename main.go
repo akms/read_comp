9d6023a4cbf0dacdc687eaef8913c20f6425d2fb
akimasa <0155take@gmail.com>
2015-06-03 22:39:37 +0900

    goroutine

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"archive/tar"
-	"bytes"
-	"compress/gzip"
-	"fmt"
 	"github.com/codegangsta/cli"
-	"io"
 	"log"
 	"os"
-	"path/filepath"
-	"regexp"
 	"read_comp/tarreader"
 )
 
@@ -44,50 +37,7 @@ OPTIONS:
 		}
 
 		i := c.Args()
-		go readFile(i)
+		tarreader.Readfile(i)
 	}
 	app.Run(os.Args)
-}
-
-func readFile(args []string) {
-	var (
-		buf         bytes.Buffer
-		fileReader  io.ReadCloser
-		file        *os.File
-		file_name   string
-		target_name string
-		err         error
-		hdr         *tar.Header
-		dir_name    string
-		fileinfo    os.FileInfo
-		body        []byte
-	)
-	dd, _ := os.Getwd()
-	dir_name, file_name = filepath.Split(args[0])
-	changeDir(dir_name)
-	target_name = args[1]
-	default_Regexp := regexp.MustCompile(target_name)
-	file, err = os.Open(file_name)
-	if err != nil {
-		log.Fatal("Can'ft open file \n")
-	}
-	defer file.Close()
-
-	_, err = io.Copy(&buf, file)
-	if fileReader, err = gzip.NewReader(&buf); err != nil {
-		log.Fatal(err)
-	}
-	defer fileReader.Close()
-	
-	tr := tar.NewReader(fileReader)
-}
-
-func changeDir(dir_name string) {
-	var err error
-	if dir_name != "" {
-		err = os.Chdir(dir_name)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
 }
--- tarreader/tarreader.go
+++ tarreader/tarreader.go
@@ -1,10 +1,87 @@
 package tarreader
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
 )
 
-func tarreader() {
+var (
+	reader chan *tar.Reader = make(chan *tar.Reader)
+	header chan *tar.Header = make(chan *tar.Header)
+	body        []byte
+	fileinfo    os.FileInfo
+)
+
+func unCompress() {
+	tr := <-reader
+	hdr := <-header
+	fileinfo = hdr.FileInfo()
+	func() {
+		mkdir_name, _ := filepath.Split(hdr.Name)
+
+		if err := os.MkdirAll(mkdir_name, fileinfo.Mode()); err != nil {
+			log.Fatal(err)
+		}
+		if hdr.Typeflag == '0' {
+			wfile, werr := os.Create(hdr.Name)
+			if werr != nil {
+				log.Fatal(werr)
+			}
+			defer wfile.Close()
+
+			body = make([]byte, 8192)
+			for {
+				c, rerr := tr.Read(body)
+				if c == 0 {
+					break
+				}
+				if rerr != nil {
+					log.Fatal(rerr)
+				}
+				wfile.Write(body[:c])
+			}
+		}
+	}()
+}
+
+func Readfile(args []string) {
+	var (
+		buf         bytes.Buffer
+		fileReader  io.ReadCloser
+		file        *os.File
+		file_name   string
+		target_name string
+		err         error
+		hdr         *tar.Header
+		dir_name    string
+		tr          *tar.Reader
+	)
+	go unCompress()
+	dd, _ := os.Getwd()
+	dir_name, file_name = filepath.Split(args[0])
+	changeDir(dir_name)
+	target_name = args[1]
+	default_Regexp := regexp.MustCompile(target_name)
+	file, err = os.Open(file_name)
+	if err != nil {
+		log.Fatal("Can'ft open file \n")
+	}
+	defer file.Close()
+
+	_, err = io.Copy(&buf, file)
+	if fileReader, err = gzip.NewReader(&buf); err != nil {
+		log.Fatal(err)
+	}
+	defer fileReader.Close()
+
+	tr = tar.NewReader(fileReader)
 	for {
 		hdr, err = tr.Next()
 		if err == io.EOF {
@@ -15,36 +92,21 @@ func tarreader() {
 			break
 		}
 		if default_Regexp.MatchString(hdr.Name) {
+			changeDir(dd)
 			fmt.Println(hdr.Name)
-			fileinfo = hdr.FileInfo()
-			func() {
-				mkdir_name, _ := filepath.Split(hdr.Name)
-				changeDir(dd)
-
-				if err = os.MkdirAll(mkdir_name, fileinfo.Mode()); err != nil {
-					log.Fatal(err)
-				}
-				if hdr.Typeflag == '0' {
-					wfile, werr := os.Create(hdr.Name)
-					if werr != nil {
-						log.Fatal(werr)
-					}
-					defer wfile.Close()
-
-					body = make([]byte, 8192)
-					for {
-						c, rerr := tr.Read(body)
-						if c == 0 {
-							break
-						}
-						if rerr != nil {
-							log.Fatal(rerr)
-						}
-						wfile.Write(body[:c])
-					}
-				}
-			}()
+			reader <- tr
+			header <- hdr
 			changeDir(dir_name)
+		}
+	}
+}
+
+func changeDir(dir_name string) {
+	var err error
+	if dir_name != "" {
+		err = os.Chdir(dir_name)
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
 }
